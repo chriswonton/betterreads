3d980baff7df9133ad58d0a2d414bcdb709f1f5a
Chris Ton-That <69617940+chriswonton@users.noreply.github.com>
2023-05-02 23:14:22 -0700

    Add files via upload

--- components/Book.js
+++ components/Book.js
@@ -1,10 +1,17 @@
-import styled from 'styled-components'
+import styled from "styled-components"
 
 export default function Book({ title, author, pages, link}) {
-    return <article>
+    return <BookWrapper>
         <h1>{title}</h1>
         <h2>by {author}</h2>
-        <p>Number of Pages: {pages}</p>
-        <a href={link}>Learn More</a>
-    </article>
+        <p> number of pages: {pages}</p>
+        <a href={link}>Learn more</a>
+    </BookWrapper>
 }
+
+const BookWrapper = styled.article`
+    border: 2px solid black;
+    border-radius: 5px;
+    padding: 10px;
+    margin: 2px;
+`
